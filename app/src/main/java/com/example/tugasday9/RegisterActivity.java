ffd92d8273a1cfd31248c69e846f3d59900e43eb
dedenaifa <dedenaifah675@gmail.com>
2024-05-07 20:40:55 +0700

    .

---
+++ app/src/main/java/com/example/tugasday9/RegisterActivity.java
@@ -0,0 +1,72 @@
+package com.example.tugasday9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tugasday9.data.ApiClient;
+import com.example.tugasday9.data.ApiInterface;
+import com.example.tugasday9.data.model.Register;
+import com.example.tugasday9.databinding.ActivityRegisterBinding;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityRegisterBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btl.setOnClickListener(this);
+        binding.btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btl) {
+            moveToLogin();
+        } else if (v.getId() == R.id.btn) {
+            String username = binding.username.getText().toString();
+            String name = binding.nama.getText().toString();
+            String password = binding.password.getText().toString();
+            register(username, name, password);
+        }
+    }
+
+    private void register(String username, String name, String password) {
+        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        Call<Register> call = apiInterface.RegisterResponse(username, name, password);
+        call.enqueue(new Callback<Register>() {
+            @Override
+            public void onResponse(Call<Register> call, Response<Register> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    moveToLogin();
+                    Toast.makeText(RegisterActivity.this, "Akun Berhasil Dibuat", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Gagal membuat akun", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Register> call, Throwable t) {
+                Toast.makeText(RegisterActivity.this, "Gagal membuat akun: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void moveToLogin() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
